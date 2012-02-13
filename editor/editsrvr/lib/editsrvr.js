a8c5105ac392aa1632dac5b93faf43371ef7d24b
hugow <hugo.windisch@gmail.com>
2012-02-13 13:29:09 -0500

    Added support for extending the meatgrinder server as an edit server (adding editor specific urls).

---
+++ editor/editsrvr/lib/editsrvr.js
@@ -0,0 +1,28 @@
+#!/usr/local/bin/node
+/**
+    editsrvr.js
+    Copyright (c) Hugo Windisch 2012 All Rights Reserved
+*/
+var meatgrinder = require('meatgrinder');
+
+function serveVisual(req, res, options) {
+    console.log('serveVisual ');
+    console.log(req);
+}
+
+function getUrls(options) {
+    var urls = meatgrinder.getUrls(options);
+    urls.push({
+        filter: /^\/visual\/(.*)$/, 
+        handler: function (req, res, match) {
+            serveVisual(req, res, match, options);
+        }
+    });
+    return urls;
+}
+
+meatgrinder.serve(
+    getUrls(
+        meatgrinder.processArgs(process.argv.slice(2))
+    )
+);
