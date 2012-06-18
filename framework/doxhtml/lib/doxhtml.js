5c7103eb4c349045242828754c1bbdf7594c88aa
hugow <hugo.windisch@gmail.com>
2012-06-18 16:58:29 -0400

    Improved the dox -> html conversion.

--- framework/doxhtml/lib/doxhtml.js
+++ framework/doxhtml/lib/doxhtml.js
@@ -37,23 +37,63 @@ function jsonToHtml(json) {
     forEach(json, function (item) {
         var ctx = item.ctx,
             descr = item.description,
+            params = [],
+            fname,
+            disp,
             txt;
         if (ctx &&
                 (ctx.type === 'function' || ctx.type === 'method') &&
                 !item.isPrivate &&
                 !item.ignore) {
-            html += '<h1>' + item.ctx.string + '</h1>';
+            // extract all the parameters
             forEach(item.tags, function (t) {
                 if (t.type === 'param') {
-                    html += '<p> (' + t.types[0] + ') ' + t.name + ' ' + t.description + '</p>';
+                    params.push(t);
                 }
             });
+            // display the function name
+            fname = ctx.string.split('(')[0];
+            html += '<h1>' + fname + '(';
+            if (params.length > 0) {
+                forEach(params, function (p, i) {
+                    html += p.name;
+                    if (i !== params.length - 1) {
+                        html += ', ';
+                    }
+                });
+            }
+            html += ')</h1>';
+            // display the description
             if (descr) {
                 txt = descr.full || descr.summary;
                 if (txt) {
+                    html += '<h2>Description</h2>';
                     html += '<p>' + txt + '</p>';
                 }
             }
+            // display the parameters
+            disp = false;
+            forEach(item.tags, function (t) {
+                if (t.type === 'param') {
+                    if (!disp) {
+                        html += '<h2>Parameters</h2>';
+                        disp = true;
+                    }
+                    html += '<b>' + t.name + '</b> (' + t.types[0] + ') : ' + t.description + '</p>';
+                }
+            });
+            // display the return value
+            disp = false;
+            forEach(item.tags, function (t) {
+                if (t.type === 'returns') {
+                    if (!disp) {
+                        html += '<h2>Returns</h2>';
+                        disp = true;
+                    }
+                    html += '<p>' + t.string + '</p>';
+                }
+            });
+            html += '<hr>';
         }
     });
     return html;
