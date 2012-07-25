3fd47b0154e695270b69bc5e20f1a7c6e1160acf
hugow <hugo.windisch@gmail.com>
2012-07-24 20:12:34 -0400

    Small jslint related fix.

--- framework/events/test/test.js
+++ framework/events/test/test.js
@@ -20,6 +20,7 @@
     FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
     IN THE SOFTWARE.
 */
+/*jslint nomen: false */
 var events = require('events'),
     assert = require('assert');
 
